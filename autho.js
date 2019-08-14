a38a2e2fd2734c705e623111aaf6375773c3c10e
Elena <53603781+LonelyDutchhound@users.noreply.github.com>
2019-08-15 02:14:20 +0300

    Add files via upload

---
+++ autho.js
@@ -0,0 +1,30 @@
+$('a.button')[0].addEventListener('click',  authorizeUser);
+let firstName, lastName, userId, friends;
+
+function authorizeUser() {
+VK.init({
+    apiId: 7096654
+});
+VK.Auth.login(response => {
+
+    if (response.session) {
+        firstName = response.session.user.first_name;
+        lastName = response.session.user.last_name;
+        userId = response.session.user.id;
+           $('.container').html('<h1>Привет '+ firstName +' ' + lastName + ' !<h1>');
+
+        VK.Api.call('friends.get', { fields: 'photo_rec', order: 'random', count:5 , v:'5.8'}, function(data){
+            $('.friendlist').html('Твои друзья:<ul></ul>')
+
+              friends = data.response.items.map(function (a){
+                return '<li>' + a.first_name+' '+a.last_name + '</li>';
+              });
+
+              $('ul').html(friends);
+        });
+
+    } else {
+      $('#container').html('<p> Authorization failed... </p>');
+    }
+  });
+}

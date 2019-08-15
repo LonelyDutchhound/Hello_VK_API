b36ef3d9b05c1f497bd7d56c9e6f2b91460537b5
Lonely Dutchhound <lonely.dutchhound@gmail.com>
2019-08-15 16:42:16 +0300

    design+pics

--- autho.js
+++ autho.js
@@ -1,30 +1,65 @@
-$('a.button')[0].addEventListener('click',  authorizeUser);
-let firstName, lastName, userId, friends;
+"use strict";
+
+var userId, firstName, lastName, userPhoto, friends;
+let cookieName = "vk_app_7096654";
+
+let userCookie = checkCookie(cookieName);
+
+function checkCookie(cookieName) {
+  let result = document.cookie.match('(^|;) ?' + cookieName + '=([^;]*)(;|$)');
+  if (result)
+    return true;
+  else
+    return false;
+}
+
+if (userCookie === false) {
+  $('.btn-container').html('<a class="button">Авторизироваться через VK<a>');
+  $('.button')[0].addEventListener('click', authorizeUser);
+} else {
+  authorizeUser();
+}
 
 function authorizeUser() {
-VK.init({
-    apiId: *******
-});
-VK.Auth.login(response => {
+
+  VK.init({
+    apiId: 7096654
+  });
+
+  VK.Auth.login(response => {
 
     if (response.session) {
-        firstName = response.session.user.first_name;
-        lastName = response.session.user.last_name;
-        userId = response.session.user.id;
-           $('.container').html('<h1>Привет '+ firstName +' ' + lastName + ' !<h1>');
-
-        VK.Api.call('friends.get', { fields: 'photo_rec', order: 'random', count:5 , v:'5.8'}, function(data){
-            $('.friendlist').html('<p>Твои друзья:</p><ul></ul>')
-
-              friends = data.response.items.map(function (a){
-                return '<li>' + a.first_name+' '+a.last_name + '</li>';
-              });
-
-              $('ul').html(friends);
-        });
-
+      firstName = response.session.user.first_name;
+      lastName = response.session.user.last_name;
+      userId = response.session.user.id;
     } else {
-      $('#container').html('<p> Authorization failed... </p>');
-    }
+      $('.app-container').html('<p> Authorization failed... </p>');
+    };
+
+
+    VK.Api.call('users.get', {
+      user_id: userId,
+      fields: 'photo_100',
+      v: '5.89'
+    }, function(data){
+        userPhoto = data.response[0].photo_100;
+         $('.greeting-text').html('<p><img src="' + userPhoto + '"/>Привет ' + firstName + ' ' + lastName + ' !<p>');
+    });
+
+
+
+    VK.Api.call('friends.get', {
+      fields: 'photo_50',
+      order: 'random',
+      count: 5,
+      v: '5.8'
+    }, function(data) {
+
+      friends = data.response.items.map(function(a) {
+        return '<li class="friend">' + a.first_name + ' ' + a.last_name + '<img src="' + a.photo_50 + '"></li>';
+      });
+
+      $('ul.friendlist').html(friends);
+    });
   });
 }
